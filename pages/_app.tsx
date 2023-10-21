028610a62a8f1df6d6eeb6a723313d00d5ecd543
0xhardman <1142783160@qq.com>
2023-10-21 13:54:52 +0800

    metamask-sdk  connect wallet

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,9 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { MetaMaskContextProvider } from '@/utils/hooks/useMetaMask';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <MetaMaskContextProvider>
+    <Component {...pageProps} />
+  </MetaMaskContextProvider>
 }
