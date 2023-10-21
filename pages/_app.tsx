b009a102a02055757e5d62a0ab9f3b7e499db1f4
0xhardman <1142783160@qq.com>
2023-10-22 01:15:58 +0800

    add contract interact tool

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,46 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import '@/styles/globals.css';
+import type { AppProps } from 'next/app';
+
+import '@rainbow-me/rainbowkit/styles.css';
+import { getDefaultWallets, RainbowKitProvider } from '@rainbow-me/rainbowkit';
+import { configureChains, createConfig, sepolia, WagmiConfig } from 'wagmi';
+import { mainnet, polygon, polygonMumbai, goerli, optimism, arbitrum, zora, arbitrumGoerli, optimismGoerli } from 'wagmi/chains';
+import { alchemyProvider } from 'wagmi/providers/alchemy';
+import { publicProvider } from 'wagmi/providers/public';
+import Script from 'next/script';
+// import { Linea } from '@/utils/linea';
 import { MetaMaskContextProvider } from '@/utils/hooks/useMetaMask';
 
+const { chains, publicClient } = configureChains(
+  [mainnet, optimism, polygon, arbitrum, goerli, polygonMumbai, sepolia, optimismGoerli],
+  // [mainnet, polygon, optimism, arbitrum, zora],
+  [
+    // alchemyProvider({ apiKey: process.env.ALCHEMY_ID }),
+    publicProvider(),
+  ]
+);
+
+const { connectors } = getDefaultWallets({
+  appName: 'multi-donate',
+  projectId: '5aa05da3efe2d1f2fbd832d55bc2b4ee',
+  chains,
+});
+
+const wagmiConfig = createConfig({
+  autoConnect: true,
+  connectors,
+  publicClient,
+});
+
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <MetaMaskContextProvider>
-    <Component {...pageProps} />
-  </MetaMaskContextProvider>
+  return (
+    <MetaMaskContextProvider>
+      <WagmiConfig config={wagmiConfig}>
+        <RainbowKitProvider chains={chains}>
+          <Component {...pageProps} />
+        </RainbowKitProvider>
+      </WagmiConfig>
+    </MetaMaskContextProvider>
+  );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,18 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import { MetaMaskConnectBtn } from '@/components/MetaMaskConnectBtn'
+import { useAccount } from 'wagmi'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
+  const { address } = useAccount()
+  console.log(address)
   return (
     <main
       className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
     >
+
       <MetaMaskConnectBtn />
     </main>
   )
